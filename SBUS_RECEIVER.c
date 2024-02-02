ad477d67ed03091940ae99a4399d68868099c22f
PatricioGarces <patricio.garces@outlook.com>
2024-02-01 21:48:49 -0300

    Creacion SBUS Component funcionando

---
+++ SBUS_RECEIVER.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include "include/SBUS_RECEIVER.h"
+#include "driver/uart.h"
+#include "string.h"
+#include "esp_log.h"
+#include "freertos/queue.h"
+
+#define TAG "SBUS_RECEIVER"
+
+const int rxPin = 16;
+const int txPin = 17;
+const int uartPort= UART_NUM_2;
+
+QueueHandle_t queueNewSBUS;
+
+uint8_t rawToPercent(uint16_t value){
+    return ((value - CHANNEL_MIN) *100.00) / (CHANNEL_MAX-CHANNEL_MIN);
+}
+
+int8_t rawToSwith(uint16_t value){
+    if(value > (SN_POS_1_VALUE - 5) && value < (SN_POS_1_VALUE + 5)){
+        return SN_POS_1;
+    }
+    else if(value > (SN_POS_2_VALUE - 5) && value < (SN_POS_2_VALUE + 5)){
+        return SN_POS_2;
+    }
+    else if(value > (SN_POS_3_VALUE - 5) && value < (SN_POS_3_VALUE + 5)){
+        return SN_POS_3;
+    }
+    return -1;
+}
+
+void receiveTask(void *pvParameters ){
+
+    uint8_t data[24],length = 0;
+    channels_control_t newControl;
+    int16_t channels[17];
+
+    queueNewSBUS = xQueueCreate(1,sizeof(newControl));
+
+    while(1){      
+        uart_get_buffered_data_len(uartPort, (size_t*)&length);          // obtengo datos para leer
+        
+        if(length > 0){    
+            length = uart_read_bytes(uartPort,data,length,0);
+
+            // esp_log_buffer_hex(TAG, &data, length);
+
+            if(data[0] == 0x0F && length == 25 && data[length] == 0x00){
+
+                channels[0]  = (uint16_t) ((data[1] | data[2] <<8) & 0x07FF);
+                channels[1]  = ((data[2]>>3 |data[3]<<5)  & 0x07FF);
+                channels[2]  = ((data[3]>>6 |data[4]<<2 |data[5]<<10)  & 0x07FF);
+                channels[3]  = ((data[5]>>1 |data[6]<<7) & 0x07FF);
+                channels[4]  = ((data[6]>>4 |data[7]<<4) & 0x07FF);
+                channels[5]  = ((data[7]>>7 |data[8]<<1 |data[9]<<9)   & 0x07FF);
+                channels[6]  = ((data[9]>>2 |data[10]<<6) & 0x07FF);
+                channels[7]  = ((data[10]>>5|data[11]<<3) & 0x07FF);
+                channels[8]  = ((data[12]   |data[13]<<8) & 0x07FF);
+                channels[9]  = ((data[13]>>3|data[14]<<5)  & 0x07FF);
+                channels[10] = ((data[14]>>6|data[15]<<2|data[16]<<10) & 0x07FF);
+                channels[11] = ((data[16]>>1|data[17]<<7) & 0x07FF);
+                channels[12] = ((data[17]>>4|data[18]<<4) & 0x07FF);
+                channels[13] = ((data[18]>>7|data[19]<<1|data[20]<<9)  & 0x07FF);
+                channels[14] = ((data[20]>>2|data[21]<<6) & 0x07FF);
+                channels[15] = ((data[21]>>5|data[22]<<3) & 0x07FF);
+                channels[16] = ((data[23]));
+
+                // printf("1: %d,2: %d,3: %d,4: %d,5: %d,6: %d,7: %d\n",rawToPercent(channels[0]),rawToPercent(channels[1]),rawToPercent(channels[2]),rawToPercent(channels[3]),rawToPercent(channels[4]),rawToSwith(channels[5]),rawToSwith(channels[6]));
+
+                newControl.throttle = rawToPercent(channels[THROTTLE_CHANNEL]);
+                newControl.aileron = rawToPercent(channels[AILERON_CHANNEL]);
+                newControl.rudder = rawToPercent(channels[RUDDER_CHANNEL]);
+                newControl.elevator = rawToPercent(channels[ELEVATOR_CHANNEL]);
+                newControl.p1 = rawToPercent(channels[P1_CHANNEL]);
+                newControl.s1 = rawToSwith(channels[S1_CHANNEL]);
+                newControl.s2 = rawToSwith(channels[S2_CHANNEL]);
+                newControl.err = 0;
+
+                xQueueSend(queueNewSBUS,&newControl,0);
+            
+            }
+            else{
+                ESP_LOGE(TAG,"RESYNC");
+                uart_flush(uartPort);
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(14));
+    }
+}
+
+void testReceiveQueue(){
+
+    channels_control_t newControlMessage;
+    while(true){
+
+        if( xQueueReceive(queueNewSBUS,&newControlMessage,0)){
+            printf("throttle: %d,aileron: %d,elevator: %d,rudder: %d,p1: %d,s1: %d,s2: %d\n",newControlMessage.throttle,newControlMessage.aileron,newControlMessage.elevator,newControlMessage.rudder,newControlMessage.p1,newControlMessage.s1,newControlMessage.s2);
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
+void sbusInit(void){
+
+    /*configuro uart*/
+    uart_config_t uart_config={
+        .baud_rate = 100000,
+        .data_bits = UART_DATA_8_BITS,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .parity = UART_PARITY_EVEN,
+        .rx_flow_ctrl_thresh = 112,
+        .stop_bits = UART_STOP_BITS_2,
+        .source_clk = UART_SCLK_DEFAULT,//UART_SCLK_APB,//UART_SCLK_REF_TICK,
+
+    };
+    uart_param_config(uartPort,&uart_config);
+
+    /* configuro pines a utilizar por el uart*/
+    uart_set_pin(uartPort,txPin,rxPin,-1,-1);
+
+    uart_set_line_inverse(uartPort,UART_SIGNAL_RXD_INV);
+
+    const int uart_buffer_size = (1024 * 2);
+    QueueHandle_t uart_queue;
+    // Install UART driver using an event queue here
+    uart_driver_install(uartPort, uart_buffer_size, uart_buffer_size, 10, &uart_queue, 0);
+
+    xTaskCreate(&receiveTask,"tarea recepcion", 4096, NULL, 3 , NULL);
+
+    xTaskCreate(&testReceiveQueue,"tarea recepcion", 4096, NULL, 3 , NULL);
+}
---
+++ include/SBUS_RECEIVER.h
@@ -0,0 +1,40 @@
+#ifndef __SBUS_RECEIVER_H__
+#define __SBUS_RECEIVER_H__
+
+#include "stdio.h"
+
+#define THROTTLE_CHANNEL    2
+#define AILERON_CHANNEL     0
+#define RUDDER_CHANNEL      3
+#define ELEVATOR_CHANNEL    1
+#define P1_CHANNEL          4
+#define S1_CHANNEL          5
+#define S2_CHANNEL          6
+
+#define CHANNEL_MIN         364    
+#define CHANNEL_MAX         1684    
+
+#define SN_POS_1_VALUE      1541    
+#define SN_POS_2_VALUE      1024   
+#define SN_POS_3_VALUE      511     
+
+enum{
+    SN_POS_1,
+    SN_POS_2,
+    SN_POS_3
+};
+
+typedef struct{
+    uint8_t throttle;
+    uint8_t aileron;
+    uint8_t rudder;
+    uint8_t elevator;
+    uint8_t p1;
+    uint8_t s1;
+    uint8_t s2;
+    uint8_t err;
+}channels_control_t;
+
+void sbusInit(void);
+
+#endif
